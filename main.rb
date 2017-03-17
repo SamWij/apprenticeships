e3c766685722e0dc4b10e9b586ed69fd023cda2f
SamWij <sam.wijesinha@gmail.com>
2017-03-17 12:21:52 +1100

    login issues

--- main.rb
+++ main.rb
@@ -68,7 +68,7 @@ post '/apprentice' do
   new_user.password = params[:password]
 
   if new_user.save
-    redirect "/view_apprentice/#{new_user.id}"
+    redirect "/"
   else
     erb :index
   end
@@ -77,7 +77,7 @@ end
 
 
 post '/apprentice_cv' do
-  new_user = session[:id]
+
   #workPreferences
   work_pref = WorkPreferences.new
   work_pref.apprentice_profile_id = session[:id]
